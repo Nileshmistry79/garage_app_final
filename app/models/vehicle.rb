9e0e57e3e81d5394b10ab5a7fba3fc7e25dcc2ca
Nilesh Ruby Rails dummy projects <nilesh.mistry79@gmail.com>
2017-01-07 12:35:31 +0000

    Vehcile validation

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle < ActiveRecord::Base
-      validates_format_of :vehcile_no, :with => /^[A-Za-z]{2}[a-zA-Z0-9]{3,}$
+      validates :vehcile_no, uniqueness: { case_sensitive: false}
+      validates_format_of :vehcile_no, :with => /\A^[A-Za-z]{2}[a-zA-Z0-9]{3,}$\z/, messages: "Invalid Vehcile Number format"
       validates :Model_name, presence: true, length: {minimum:3, maximum:10}
 end
